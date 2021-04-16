aa67846681290e5511d8d601497eedf1a48c9a18
cyc327 <ycc880512@gmail.com>
2021-04-16 14:15:51 +0800

    first commit

--- ML_midproject.py
+++ ML_midproject.py
@@ -1,10 +1,12 @@
 import math
 import random
 for i in range(20):
-    velocity = random.randint(0,100)
-    angle = math.radians(random.randint(0,91))
+    velocity = random.uniform(0,100)
+    angle = math.radians(random.randint(1,91))
     xi = velocity*math.cos(angle)
     yi = velocity*math.sin(angle)
     H = (velocity**2)*(math.sin(angle)**2)/(2*9.8)
     R = (velocity**2)*(math.sin(angle*2))/9.8
-    print(round(H,3), round(R,3))
+    print("v =","{:8.4f}".format(velocity), "a =", "{:8.4f}".format(angle))    
+    print("H =","{:8.3f}".format(H), "R =","{:8.3f}".format(R))
+    print("\t")
